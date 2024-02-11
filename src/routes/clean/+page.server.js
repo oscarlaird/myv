d97bee2a66028d2a7b167b7a2b6f6a71271b10ea
Oscar Laird <olaird25@gmail.com>
2024-02-10 23:35:28 -0500

    first commit

---
+++ src/routes/clean/+page.server.js
@@ -0,0 +1,16 @@
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+        cookies.set('user', data.get('user'), { path: '/' });
+
+	}
+};
+
+export function load({ cookies }) {
+	const user = cookies.get('user');
+
+	return {
+		dummy: 'dummy',
+		user: user
+	};
+}
